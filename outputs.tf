84cc647e26978fc10cfe7553a611926b00c77734
jstehlik-ohpen <90175106+jstehlik-ohpen@users.noreply.github.com>
2022-05-31 09:00:01 +0200

    feat: LANZ-2500 Add and expose read and write policies (#21)

--- outputs.tf
+++ outputs.tf
@@ -6,12 +6,24 @@ output "namespace_arn" {
   value = try(lookup(local.created_namespace, var.namespace_type).arn, null)
 }
 
+output "namespace_name" {
+  value = try(var.name, null)
+}
+
 output "namespace_hosted_zone_id" {
   value = try(lookup(local.created_namespace, var.namespace_type).hosted_zone, null)
 }
 
+output "iam_read_only_policy_arn" {
+  value = try(aws_iam_policy.iam_read_only_policy.arn, null)
+}
+
 output "iam_read_only_role_arn" {
   value = try(var.create_iam_read_only_role ? aws_iam_role.iam_read_only_role[0].arn : null, null)
+}
+
+output "iam_read_write_policy_arn" {
+  value = try(aws_iam_policy.iam_read_write_policy.arn, null)
 }
 
 output "iam_read_write_role_arn" {
