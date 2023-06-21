df424fbec5abf7503058242c7ea4bc87a0273761
Jozef Cajkovic <jozef.cajkovic@gmail.com>
2023-06-21 20:12:04 +0200

    chore: LANZ-3974 add baseline tags

--- namespace.tf
+++ namespace.tf
@@ -2,14 +2,14 @@ resource "aws_service_discovery_http_namespace" "namespace" {
   count = var.type == "http" ? 1 : 0
   name = var.name
   description = local.description
-  tags = var.tags
+  tags = local.baseline_tags
 }
 
 resource "aws_service_discovery_private_dns_namespace" "namespace" {
   count = var.type == "private_dns" ? 1 : 0
   name = var.name
   description = local.description
-  tags = var.tags
+  tags = local.baseline_tags
   vpc = var.vpc_id
 }
 
@@ -17,5 +17,5 @@ resource "aws_service_discovery_public_dns_namespace" "namespace" {
   count = var.type == "public_dns" ? 1 : 0
   name = var.name
   description = local.description
-  tags = var.tags
+  tags = local.baseline_tags
 }
